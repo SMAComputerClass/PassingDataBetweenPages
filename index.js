c8f14f86431a712b469286d304e3b45a32d6b228
Jon Pawelko <JonPawelko@yahoo.com>
2018-03-02 15:50:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//'use strict';
+// Enter your js code Here
+
+var myButton = document.getElementById("myButton");
+
+myButton.addEventListener('click', function(e)
+{
+      localStorage.setItem("Player", "Jonny");
+});
---
+++ index2.js
@@ -0,0 +1,10 @@
+//'use strict';
+// Enter your js code Here
+
+var myButton2 = document.getElementById("myButton2");
+
+myButton2.addEventListener('click', function(e)
+{
+      alert (localStorage.getItem("Player"));
+
+});
